7d938c45aedc5f06886c38d77066f1dea8136a88
Pankaj Anadure <pankaj.anadure@wissen.com>
2024-02-26 14:53:52 +0530

    Abc by Ram

--- Abc.java
+++ Abc.java
@@ -2,6 +2,7 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		System.out.println("Have a good day :)");	
+		System.out.println("Have a good day :)");
+		Systme.out.println("RAM : My code is better");	
 	}
 }
