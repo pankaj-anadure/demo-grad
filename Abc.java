10cd6bdaf53321aea5dacdb983409eb6f616bdda
pankaj-anadure <80245574+pankaj-anadure@users.noreply.github.com>
2024-02-29 10:49:03 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -3,6 +3,6 @@ public class Abc
 	public static void main(String args[])
 	{
 		for(int i=1; i<=10; i++)
-			System.out.println("Have a good day :)");	
+			System.out.println("Good Morning ! Have a good day :) "+i);	
 	}
 }
